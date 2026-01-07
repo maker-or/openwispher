4f9a15562fb7c468ba1305efc3f9883a0a0ebea7
Harshith Pasupuleti <harshith10295032@gmail.com>
2026-01-07 13:43:57 +0530

    Initial Commit

---
+++ dhavnii/dhavniiApp.swift
@@ -0,0 +1,17 @@
+//
+//  dhavniiApp.swift
+//  dhavnii
+//
+//  Created by Harshith Pasupuleti on 07/01/26.
+//
+
+import SwiftUI
+
+@main
+struct dhavniiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
